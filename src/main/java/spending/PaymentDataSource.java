cbcad1cdb95f49921cc5cb545f346d42ca44d0e5
Saad Ismail <saad3112@gmail.com>
2020-04-04 15:05:34 +0500

    add PaymentsOfCurrentAndPreviousMonth

--- src/main/java/spending/PaymentDataSource.java
+++ src/main/java/spending/PaymentDataSource.java
@@ -1,12 +1,9 @@
 package spending;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PaymentDataSource {
-    public static List<Payment> fetch(long userId, int year, int month) {
-        List<Payment> payments = new ArrayList<Payment>();
+    public static PaymentsOfCurrentAndPreviousMonth fetch(long userId) {
+        PaymentsOfCurrentAndPreviousMonth paymentsOfCurrentAndPreviousMonth = new PaymentsOfCurrentAndPreviousMonth();
 
-        return payments;
+        return paymentsOfCurrentAndPreviousMonth;
     }
 }
--- src/test/java/spending/TriggersUnusualSpendingEmailTest.java
+++ src/test/java/spending/TriggersUnusualSpendingEmailTest.java
@@ -2,17 +2,13 @@ package spending;
 
 import org.junit.Test;
 
-import java.util.List;
-
 public class TriggersUnusualSpendingEmailTest {
 
     @Test
     public void triggerTest() {
         long userId = 1;
-        int year = 2016;
-        int month = 8;
 
-        List<Payment> payments = PaymentDataSource.fetch(userId, year, month);
+        PaymentsOfCurrentAndPreviousMonth paymentsOfCurrentAndPreviousMonth = PaymentDataSource.fetch(userId);
     }
 
 }
