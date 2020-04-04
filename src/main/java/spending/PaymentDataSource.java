08a7d4ab256f4092634b7a6592c7ca926e2365c6
Saad Ismail <saad3112@gmail.com>
2020-04-04 14:57:21 +0500

    add Payment fetch method

---
+++ src/main/java/spending/PaymentDataSource.java
@@ -0,0 +1,12 @@
+package spending;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentDataSource {
+    public static List<Payment> fetch(long userId, int year, int month) {
+        List<Payment> payments = new ArrayList<Payment>();
+
+        return payments;
+    }
+}
